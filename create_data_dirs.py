446ab42c287c2df97465d5a3b628dd0d27265959
pedro junqueira <pedrocj@gmail.com>
2023-07-05 07:16:09 +0930

    updated create_data_dirs to create all needed paths

--- create_data_dirs.py
+++ create_data_dirs.py
@@ -2,19 +2,18 @@ from pathlib import Path
 
 
 def create_data_subdirectories():
-    subdirectories = ['database', 'processed', 'source']
-
     current_directory = Path.cwd()
 
-    # Check if subdirectories exist, and create them if they don't
+    subdirs = [
+        'source/abs/history',
+        'source/rba/history',
+        'processed/history',
+        'database',
+    ]
 
-    for subdir in subdirectories:
-        new_directory = current_directory / subdir
-        if new_directory.exists():
-            print(f"{subdir} already exist")
-        else:
-            print(f"{subdir} created")
-            new_directory.mkdir()
+    for p in subdirs:
+        directory_path = current_directory / p
+        directory_path.mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == '__main__':
